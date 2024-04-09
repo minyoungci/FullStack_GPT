600cca3b32fd14e3c7cf670424eb32bc3cafb6ef
MINYOUNGXI <dbssus123@gmail.com>
2024-04-09 13:28:26 +0900

    test

--- recap/QuizGPT.py
+++ recap/QuizGPT.py
@@ -78,7 +78,7 @@ else:
         def set_quiz_submitted(value: bool):
             st.session_state.update({"quiz_submitted": value})
 
-        @st.cache_data(show_spinner="퀴즈를 맛있게 굽고 있어요...")
+        @st.cache_data(show_spinner="퀴즈를 만드는 중입니다...")
         def run_quiz_chain(*, subject, count, difficulty):
             chain = prompt | llm
             return chain.invoke(
